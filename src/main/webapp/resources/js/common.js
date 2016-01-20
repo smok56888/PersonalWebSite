d4567d0aa5cd135abdd60715c6f07bc11c06eeaf
smok56888 <liuwow@qq.com>
2016-01-20 18:49:31 +0800

    新增js

---
+++ src/main/webapp/resources/js/common.js
@@ -0,0 +1,68 @@
+/**
+ * Created by smok on 2016/1/20.
+ */
+//设置cookie  expTime:cookie超时时间(ms)
+function setCookie(name, value, expTime)//两个参数，一个是cookie的名子，一个是值
+{
+    var exp = new Date();
+    exp.setTime(exp.getTime() + expTime);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+function getCookie(name) {
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) return unescape(arr[2]);
+    return null;
+}
+function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
+
+
+//js实现jQuery函数
+function hasClass(ele, cls) {
+    return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+function addClass(ele, cls) {
+    if (!this.hasClass(ele, cls)) ele.className += " " + cls;
+}
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
+//获取QueryString的数组
+function getQueryString() {
+    var result = location.search.match(new RegExp("[\?\&][^\?\&]+=[^\?\&]+", "g"));
+    if (result == null) {
+        return "";
+    }
+    for (var i = 0; i < result.length; i++) {
+        result[i] = result[i].substring(1);
+    }
+    return result;
+}
+function getQueryStringByName(name) {
+    var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+    if (result == null || result.length < 1) {
+        return "";
+    }
+    return result[1];
+}
+function getQueryStringByIndex(index) {
+    if (index == null) {
+        return "";
+    }
+    var queryStringList = getQueryString();
+    if (index >= queryStringList.length) {
+        return "";
+    }
+    var result = queryStringList[index];
+    var startIndex = result.indexOf("=") + 1;
+    result = result.substring(startIndex);
+    return result;
+}
